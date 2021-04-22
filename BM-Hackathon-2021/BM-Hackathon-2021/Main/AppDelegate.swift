7f86b60324f98e85ee8308a120da83b9a101edb9
Andrew Galushka <andrewgalushka@yahoo.com>
2021-04-22 22:45:10 +0300

    Add project

---
+++ BM-Hackathon-2021/BM-Hackathon-2021/Main/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  BM-Hackathon-2021
+//
+//  Created by Andrii Halushka on 22.04.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
+
---
+++ BM-Hackathon-2021/BM-Hackathon-2021/Navigation/AppCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  AppCoordinator.swift
+//  BM-Hackathon-2021
+//
+//  Created by Andrii Halushka on 22.04.2021.
+//
+
+import UIKit
+
+class AppCoordinator {
+    private let window: UIWindow
+    private let navigationController = UINavigationController(rootViewController: UIViewController())
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        window.rootViewController = makeInitialScreen()
+        window.makeKeyAndVisible()
+    }
+}
+
+private extension AppCoordinator {
+    func makeInitialScreen() -> UIViewController {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .red
+        return vc
+    }
+}
+
+
+
