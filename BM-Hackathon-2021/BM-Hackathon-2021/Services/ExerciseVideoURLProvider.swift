19d0acdf1a8472d86999bd6bcc780424f5e5f309
Andrew Galushka <andrewgalushka@yahoo.com>
2021-04-24 17:56:11 +0300

    Add more videos

--- BM-Hackathon-2021/BM-Hackathon-2021/Services/ExerciseVideoURLProvider.swift
+++ BM-Hackathon-2021/BM-Hackathon-2021/Services/ExerciseVideoURLProvider.swift
@@ -9,15 +9,32 @@ import Foundation
 
 class ExerciseVideoURLProvider {
     static let shared = ExerciseVideoURLProvider()
-    private let videoURLs = [FileNames.allCases]
+    private let videoURLs = ExerciseVideoURLProvider.loadURLs()
+    private var currentRandomsIterator = ExerciseVideoURLProvider.loadURLs().shuffled().makeIterator()
     
     func nextURL() -> URL {
-        let url = Bundle.main.url(forResource: FileNames.allCases.randomElement()!.rawValue, withExtension: nil)!
-        return url
+        if let next = currentRandomsIterator.next() {
+            return next
+        }
+        
+        var iterator = videoURLs.shuffled().makeIterator()
+        let next = iterator.next()!
+        self.currentRandomsIterator = iterator
+        
+        return next
+    }
+    
+    private static func loadURLs() -> [URL] {
+        
+        return FileNames.allCases.map {
+            let url = Bundle.main.url(forResource: $0.rawValue, withExtension: nil)!
+            return url
+        }
     }
     
     private enum FileNames: String, CaseIterable {
         case jumpingJacks = "jumping_jacks.mp4"
         case dancingRumba = "dancing_rumba.mp4"
+        case lunges = "lunges.mp4"
     }
 }
